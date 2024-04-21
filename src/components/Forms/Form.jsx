9be912438c2efa31cbbd274f0a2f44125c2ae0f5
VorobeiIvan <0675361905vi@gmail.com>
2024-04-21 23:58:12 +0300

    fix

--- src/components/Forms/Form.jsx
+++ src/components/Forms/Form.jsx
@@ -1,19 +1,6 @@
 import Button from './Button';
 
-const Form = ({ className, buttonProps, children }) => {
-  const handleSubmit = event => {
-    event.preventDefault();
-    alert(`Form submitted`);
-    const inputs = event.target.getElementsByTagName('input');
-    for (let input of inputs) {
-      input.value = '';
-    }
-    const textarea = event.target.getElementsByTagName('textarea')[0];
-    if (textarea) {
-      textarea.value = '';
-    }
-  };
-
+const Form = ({ className, buttonProps, children, handleSubmit }) => {
   return (
     <form onSubmit={handleSubmit} className={`form ${className}`}>
       {children}
